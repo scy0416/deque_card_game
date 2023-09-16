739b1c2ca094c3766d9d10574d48d0212f401583
scy0416 <45913784+scy0416@users.noreply.github.com>
2023-09-16 20:30:20 +0900

    타이틀 씬 일부 및 플레이 씬 일부 생성

---
+++ Scripts/TitleScene.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# 시작 버튼을 누르는 경우
+func _on_start_pressed():
+	pass # Replace with function body.
+
+# 규칙 버튼을 누르는 경우
+func _on_rule_pressed():
+	pass # Replace with function body.
+
+# 나가기 버튼을 누르는 경우
+func _on_quit_pressed():
+	get_tree().quit()
