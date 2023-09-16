7a50686b73c07fc88b2e0aba87867efe9397194f
scy0416 <45913784+scy0416@users.noreply.github.com>
2023-09-16 22:39:47 +0900

    카드 숫자 변경 시 툴에서 적용되는 기능 추가및 카드 씬 완성

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -7,7 +7,11 @@ extends TextureRect
 		card_pattern = value
 		if not isJoker:
 			texture = load("res://cards/" + value + "-" + card_num + ".png")
-@export_enum("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K") var card_num:String = "A"
+@export_enum("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K") var card_num:String = "A":
+	set(value):
+		card_num = value
+		if not isJoker:
+			texture = load("res://cards/" + card_pattern + "-" + value + ".png")
 @export var isJoker:bool = false:
 	set(value):
 		isJoker = value
