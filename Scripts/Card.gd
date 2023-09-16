f1a15644bea96ba03ab2ea0da08019b7894ed94d
scy0416 <45913784+scy0416@users.noreply.github.com>
2023-09-16 22:37:37 +0900

    조커 변환 및 그림 변환 기능 완성

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -1,5 +1,17 @@
+@tool
 extends TextureRect
 
 
-@export_enum("Spade", "Diamond", "Heart", "Clover") var card_pattern:String
-@export_enum("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K") var card_num:String
+@export_enum("Spade", "Diamond", "Heart", "Clover") var card_pattern:String = "Spade":
+	set(value):
+		card_pattern = value
+		if not isJoker:
+			texture = load("res://cards/" + value + "-" + card_num + ".png")
+@export_enum("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K") var card_num:String = "A"
+@export var isJoker:bool = false:
+	set(value):
+		isJoker = value
+		if value:
+			texture = load("res://cards/Joker.png")
+		else:
+			texture = load("res://cards/" + card_pattern + "-" + card_num + ".png")
