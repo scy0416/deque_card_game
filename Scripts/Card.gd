7cfb92af76d8f0113e28c7c4465a3c60d5ad0c68
scy0416 <45913784+scy0416@users.noreply.github.com>
2023-09-17 17:57:58 +0900

    플레이 화면 구성 완료 단, 승리, 패배에 대한 처리가 아직 안됨

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -1,6 +1,7 @@
 @tool
 extends TextureRect
 
+var num_dict = {"A" = 1, "2" = 2, "3" = 3, "4" = 4, "5" = 5, "6" = 6, "7" = 7, "8" = 8, "9" = 9, "10" = 10, "J" = 11, "Q" = 12, "K" = 13}
 
 @export_enum("Spade", "Diamond", "Heart", "Clover") var card_pattern:String = "Spade":
 	set(value):
@@ -19,3 +20,7 @@ extends TextureRect
 			texture = load("res://cards/Joker.png")
 		else:
 			texture = load("res://cards/" + card_pattern + "-" + card_num + ".png")
+
+
+func check_puttable(target):
+	return card_pattern == target.card_pattern or num_dict[card_num] == num_dict[target.card_num] - 1 or num_dict[card_num] == num_dict[target.card_num] + 1
--- Scripts/play_scene.gd
+++ Scripts/play_scene.gd
@@ -1,9 +1,17 @@
 extends Node
+signal check_start
 
 var CardPattern = ["Spade", "Diamond", "Heart", "Clover"]
 var CardNum = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 var card:Array
+
+var card_scene:PackedScene = preload("res://Scenes/card.tscn")
+
+var remain_cards:int:
+	set(value):
+		remain_cards = value
+		$UI/VBC/Life.value = value
 
 func _ready():
 	for pattern in CardPattern:
@@ -12,6 +20,8 @@ func _ready():
 			tmp.pattern = pattern
 			tmp.num = num
 			card.append(tmp)
+	
+	remain_cards = 52
 	
 	shuffle_card()
 	#for c in card:
@@ -23,11 +33,16 @@ func _ready():
 	#$UI/VBC/Other/ReleaseCardContainer/VBC/ReleasedCard.flip_card()
 	await get_tree().create_timer(1).timeout
 	$UI/VBC/Other/ReleaseCardContainer/VBC/ReleasedCard.flip_card()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+	remain_cards -= 1
+	
+	for i in range(0, 5):
+		var c = card.pop_front()
+		append_back(c)
+		remain_cards -= 1
+		await get_tree().create_timer(1).timeout
+	
+	#start_game()
+	emit_signal("check_start")
 
 
 func shuffle_card():
@@ -38,3 +53,104 @@ func shuffle_card():
 		var temp = card[k]
 		card[k] = card[n]
 		card[n] = temp
+
+
+func append_front(c:CardInfo):
+	var card_instance = card_scene.instantiate()
+	card_instance.card_pattern = c.pattern
+	card_instance.card_num = c.num
+	$UI/VBC/Hand/ReleaseFront.add_sibling(card_instance)
+	pass
+
+
+func append_back(c:CardInfo):
+	var card_instance = card_scene.instantiate()
+	card_instance.card_pattern = c.pattern
+	card_instance.card_num = c.num
+	var nodes_size = $UI/VBC/Hand.get_child_count()
+	var target_node = $UI/VBC/Hand.get_child(nodes_size - 3)
+	target_node.add_sibling(card_instance)
+	pass
+
+
+func start_game():
+	check_state()
+
+
+func check_state():
+	var released_card = $UI/VBC/Other/ReleaseCardContainer/VBC/ReleasedCard/Card
+	var front_card = $UI/VBC/Hand.get_child(2)
+	var back_card = $UI/VBC/Hand.get_child($UI/VBC/Hand.get_child_count() - 3)
+	
+	if $UI/VBC/Hand.get_child_count() == 4:
+		pass
+	
+	var front_puttable = released_card.check_puttable(front_card)
+	var back_puttable = released_card.check_puttable(back_card)
+	
+	$UI/VBC/Hand/ReleaseFront.visible = false
+	$UI/VBC/Hand/ReleaseBack.visible = false
+	$UI/VBC/Hand/PutFront.visible = false
+	$UI/VBC/Hand/PutBack.visible = false
+	
+	if front_puttable or back_puttable:
+		$UI/VBC/Hand/ReleaseFront.visible = front_puttable
+		$UI/VBC/Hand/ReleaseBack.visible = back_puttable
+	else:
+		if remain_cards == 0:
+			pass
+		next_card_open()
+		$UI/VBC/Hand/PutFront.visible = true
+		$UI/VBC/Hand/PutBack.visible = true
+
+
+func next_card_open():
+	var flip_card = $UI/VBC/Other/CardFile/VBC/FlipCard/Card
+	var next_card = card.pop_front()
+	flip_card.card_pattern = next_card.pattern
+	flip_card.card_num = next_card.num
+	$UI/VBC/Other/CardFile/VBC/FlipCard.flip_card()
+
+
+func _on_release_front_pressed():
+	var released_card = $UI/VBC/Other/ReleaseCardContainer/VBC/ReleasedCard/Card
+	var front_card = $UI/VBC/Hand.get_child(2)
+	released_card.card_pattern = front_card.card_pattern
+	released_card.card_num = front_card.card_num
+	front_card.queue_free()
+	await get_tree().create_timer(0.01).timeout
+	emit_signal("check_start")
+
+
+func _on_release_back_pressed():
+	var released_card = $UI/VBC/Other/ReleaseCardContainer/VBC/ReleasedCard/Card
+	var back_card = $UI/VBC/Hand.get_child($UI/VBC/Hand.get_child_count() - 3)
+	released_card.card_pattern = back_card.card_pattern
+	released_card.card_num = back_card.card_num
+	back_card.queue_free()
+	await get_tree().create_timer(0.01).timeout
+	emit_signal("check_start")
+
+
+func _on_put_front_pressed():
+	var new_card = card_scene.instantiate()
+	var target = $UI/VBC/Other/CardFile/VBC/FlipCard/Card
+	new_card.card_pattern = target.card_pattern
+	new_card.card_num = target.card_num
+	$UI/VBC/Hand/ReleaseFront.add_sibling(new_card)
+	$UI/VBC/Other/CardFile/VBC/FlipCard/AnimationPlayer.stop()
+	remain_cards -= 1
+	await get_tree().create_timer(0.01).timeout
+	emit_signal("check_start")
+
+
+func _on_put_back_pressed():
+	var new_card = card_scene.instantiate()
+	var target = $UI/VBC/Other/CardFile/VBC/FlipCard/Card
+	new_card.card_pattern = target.card_pattern
+	new_card.card_num = target.card_num
+	$UI/VBC/Hand.get_child($UI/VBC/Hand.get_child_count() - 3).add_sibling(new_card)
+	$UI/VBC/Other/CardFile/VBC/FlipCard/AnimationPlayer.stop()
+	remain_cards -= 1
+	await get_tree().create_timer(0.01).timeout
+	emit_signal("check_start")
